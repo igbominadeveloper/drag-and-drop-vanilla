4ecf266d888d38bc30a1662a55206ee39df1c0dd
Favour Afolayan <favourafolayan@gmail.com>
2020-11-12 06:30:32 +0100

    drag column and store in data transfer property

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-window.addEventListener('dragstart', (event) => {
+const handleDragStart = (event) => {
   event.dataTransfer.effectAllowed = 'move';
   event.dataTransfer.dropEffect = 'move';
+
+  const childNode = event.currentTarget;
+  const parentNode = event.currentTarget.parentNode;
+
+  event.dataTransfer.setData('columnToMove', JSON.stringify(childNode));
+  event.dataTransfer.setData('sourceRow', JSON.stringify(parentNode));
   // get the row class element closest to this element
   // remove it from it
-});
+};
 
-window.addEventListener('dragend', (event) => {
+const handleDragEnd = (event) => {
   console.log('dropping now', event);
+};
+
+const columns = document.querySelectorAll('.column');
+
+columns.forEach((column) => {
+  column.addEventListener('dragstart', handleDragStart);
+  column.addEventListener('dragend', handleDragEnd);
 });
